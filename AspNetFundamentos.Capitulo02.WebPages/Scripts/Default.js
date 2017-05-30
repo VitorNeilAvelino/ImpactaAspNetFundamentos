955bb5c6c6d366cd6e0c8d65dc2d6b7715962836
Vítor Avelino <avelino.vitor@gmail.com>
2017-05-29 23:30:39 -0300

    Javascript para alternar visibilidade do elemento.

---
+++ AspNetFundamentos.Capitulo02.WebPages/Scripts/Default.js
@@ -0,0 +1,20 @@
+﻿//var dts = window.document.getElementsByTagName("dt");
+
+//for (var i = 0; i < dts.length; i++) {
+//    //dts[i].addEventListener("click", alternarVisibilidade);
+//    dts[i].addEventListener("click", function (e) { alternarVisibilidade(e.source) });
+//}
+
+var servidorP = document.getElementById("servidorP")
+servidorP.addEventListener("click", function (evento) { alternarVisibilidade(evento.srcElement.dataset.destino); });
+
+function alternarVisibilidade(elementoId) {
+    var elemento = document.getElementById(elementoId);
+
+    if (elemento.style.display == "none") {
+        elemento.style.display = "block";
+    }
+    else {
+        elemento.style.display = "none";
+    }
+}
