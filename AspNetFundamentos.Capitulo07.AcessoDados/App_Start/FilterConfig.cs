ce84d53a9f79a7d93cc7525a5ddc7c595d019fae
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-06-20 14:17:51 -0300

    LogErrorAttribute

--- AspNetFundamentos.Capitulo07.AcessoDados/App_Start/FilterConfig.cs
+++ AspNetFundamentos.Capitulo07.AcessoDados/App_Start/FilterConfig.cs
@@ -1,5 +1,5 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using AspNetFundamentos.Capitulo07.AcessoDados.Filtros;
 
 namespace AspNetFundamentos.Capitulo07.AcessoDados
 {
@@ -7,7 +7,8 @@ namespace AspNetFundamentos.Capitulo07.AcessoDados
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new LogErrorAttribute());
         }
     }
 }
---
+++ AspNetFundamentos.Capitulo07.AcessoDados/Filtros/LogErrorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace AspNetFundamentos.Capitulo07.AcessoDados.Filtros
+{
+    public class LogErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+            Debug.WriteLine( filterContext.Exception);
+        }
+    }
+}
