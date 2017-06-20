a1900d183305cd1b38347c3a9cbe3eb7b8a678c9
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2017-06-20 16:27:16 -0300

    LoginController

---
+++ AspNetFundamentos.Capitulo07.AcessoDados/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetFundamentos.Capitulo07.AcessoDados.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
